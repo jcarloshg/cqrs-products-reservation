242e0e9fd83a7751bceba0e4b28e0fc615746cde
jcarloshg <carlosj12336@gmail.com>
2025-10-17 22:47:56 -0600

    save

--- back-for-commands/test/back-for-commands/src/app/stock/confirm-reservation/domain/confirm-reservation.use-case.test.ts
+++ back-for-commands/test/back-for-commands/src/app/stock/confirm-reservation/domain/confirm-reservation.use-case.test.ts
@@ -6,17 +6,30 @@ import {
     ConfirmReservationRequest,
     ConfirmReservationUseCase,
 } from "@/app/stock/confirm-reservation/domain/confirm-reservation.use-case";
+import { ReservationStatus } from "@/app/stock/create-reservation-stock/domain/entities/reservation-stock.entity";
+import { beforeEach } from "node:test";
 
 describe("confirm-reservation.use-case.test", () => {
-    it("should be defined", async () => {
-        const confirmReservationCommandHandler = new ConfirmReservationCommandHandler();
+
+    const validUuid = "123e4567-e89b-12d3-a456-426614174000";
+    const validProductId = "987fcdeb-51a2-43d1-b123-456789abcdef";
+    const validStatus = ReservationStatus.CONFIRMED;
+
+    let useCase: ConfirmReservationUseCase;
+
+    beforeEach(() => {
+        const confirmReservationCommandHandler =
+            new ConfirmReservationCommandHandler();
         const commandHandlers = new CommandHandlers();
         commandHandlers.register(
             ConfirmReservationCommand.COMMAND_NAME,
             confirmReservationCommandHandler
         );
         const commandBus = new CommandBus(commandHandlers);
-        const useCase = new ConfirmReservationUseCase(commandBus);
+        useCase = new ConfirmReservationUseCase(commandBus);
+    });
+
+    it("should be defined", async () => {
         const request: ConfirmReservationRequest = {
             data: {
                 uuid: "asdasd",
@@ -28,6 +41,5 @@ describe("confirm-reservation.use-case.test", () => {
         expect(res).toBeDefined();
         expect(res.message).toBe("OK");
         expect(res.code).toBe(200);
-
     });
 });
